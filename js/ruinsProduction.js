5dea68404c53b5c28a23cee26327b8d07a331a66
elijaholmos <eli.olmos9@gmail.com>
2018-11-28 22:09:40 -0800

    ruins script now fully supports AGR resource cards

--- js/ruinsProduction.js
+++ js/ruinsProduction.js
@@ -60,7 +60,7 @@ function pileFilter(card) {
     // minTier = Number
     // maxTier = Number
     // productType = "standard" || "premium"
-    return (card.productType == this.productType) && (this.minTier <= card.unlockedAt <= this.maxTier);
+    return card.productType == this.productType && this.minTier <= card.unlockedAt && card.unlockedAt <= this.maxTier;
 }
 
 //okay, so since each pile has a different number of conditions to meet,
@@ -72,30 +72,35 @@ function shufflePile(pileType, pileNumber) {
     pileNumber--; //subtract one from pileNumber (Array zero index)
     console.log(pileNumber +' after');
     //first up, define the array that is going to hold the reshuffled cards
+    var unshuffledArray = [];
     var shuffledArray = [];
+    var newContents;
 
     for(var i=0; i < pileSpecs[pileType][pileNumber].length; i++) {
         //ok, do you see what I did here? since each pile has a different number
         //of conditions that need to be met, we have to get the number of conditions
         //from the pileSpecs object. 
-        var newContents = resourceCards.filter(pileFilter, pileSpecs[pileType][pileNumber][i]);
+        newContents = resourceCards.filter(pileFilter, pileSpecs[pileType][pileNumber][i]);
         console.log(newContents);
-        shuffledArray.push(...newContents);
-        console.log(shuffledArray);
-        //the above operation takes the current specs and filters them thru resourceCards,
-        //the takes the resulting array values and pushes those values to shuffledArray
-        //we use spread syntax (...) b/c we don't want to push the whole array, just its values
-
+        //the above operation takes the current specs and filters them thru resourceCards
+        
         //the next step is to look at how many of each card should be in the array
         //for that, I made this function:
-        shuffledArray = copyArrayContents(shuffledArray, pileSpecs[pileType][pileNumber].length);
-        console.log(shuffledArray);
-        //we will repeat the above operation for each pileSpecs.
+        newContents = copyArrayContents(newContents, pileSpecs[pileType][pileNumber][i].numberOfEach - 1);
+        console.log(newContents);
+
+        unshuffledArray.push(...newContents);
+        console.log(unshuffledArray);
+        //the above operation takes the resulting array values and pushes those values to shuffledArray
+        //we use spread syntax (...) b/c we don't want to push the whole array, just its values
+
+        
+        //we will repeat the loop for each pileSpecs.
     }
 
     //okay, the shuffledArray variable has all the cards in order of tier. now we need to actually shuffle it
-    console.log(shuffledArray);
-    shuffledArray.sort(function(a, b){return 0.5 - Math.random()});
+    console.log(unshuffledArray);
+    shuffledArray = unshuffledArray.sort(function(a, b){return 0.5 - Math.random()});
     console.log(shuffledArray);
     //now we need to update the piles variable with the new cards that have been shuffled
     piles[pileType][pileNumber] = shuffledArray;
@@ -105,6 +110,7 @@ function copyArrayContents(a, timesToBeCopied) {
     var b = [];
     console.log(...a);
     for(var i=0; i <= timesToBeCopied; i++) {
+        // we use <= so that the base array is included
         b.push(...a);
         console.log(b);
     }
